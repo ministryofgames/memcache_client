4a2d9e27d9711dd49c3a70e5248174ec87e27bff
Petri Kero <petri@ministryofgames.io>
2015-11-26 15:22:00 +0200

    Fix tests.

--- test/memcache_client_transcoder_test.exs
+++ test/memcache_client_transcoder_test.exs
@@ -4,20 +4,20 @@ defmodule Memcache.ClientTest.Transcoder do
   test "raw transcoder encode_value" do
     assert {:error, {:invalid_value, %{}}} == Memcache.Client.Transcoder.encode_value(%{})
 
-    {encoded, data_type} = Memcache.Client.Transcoder.encode_value("test")
+    {:ok, encoded, data_type} = Memcache.Client.Transcoder.encode_value("test")
     assert encoded == "test"
     assert data_type == 0x0000
   end
 
   test "raw transcoder decode_value" do
-    assert "test" == Memcache.Client.Transcoder.decode_value("test", 0x0000)
+    assert {:ok, "test"} == Memcache.Client.Transcoder.decode_value("test", 0x0000)
     assert {:error, {:invalid_data_type, 2}} == Memcache.Client.Transcoder.decode_value("test", 0x0002)
   end
   
   test "json transcoder encode_value" do
     Application.put_env(:memcache_client, :transcoder, Memcache.Client.Transcoder.Json)
     
-    {encoded, data_type} = Memcache.Client.Transcoder.encode_value(%{})
+    {:ok, encoded, data_type} = Memcache.Client.Transcoder.encode_value(%{})
 
     assert encoded == "{}"
     assert data_type == 0x0002
@@ -28,7 +28,7 @@ defmodule Memcache.ClientTest.Transcoder do
   test "json transocder decode_value" do
     Application.put_env(:memcache_client, :transcoder, Memcache.Client.Transcoder.Json)
     
-    assert %{} == Memcache.Client.Transcoder.decode_value("{}", 0x0002)
+    assert {:ok, %{}} == Memcache.Client.Transcoder.decode_value("{}", 0x0002)
     assert {:error, {:invalid_data_type, 0}} == Memcache.Client.Transcoder.decode_value("{}", 0x0000)
 
     Application.delete_env(:memcache_client, :transcoder)
@@ -38,7 +38,7 @@ defmodule Memcache.ClientTest.Transcoder do
     Application.put_env(:memcache_client, :transcoder, Memcache.Client.Transcoder.Json)
     Application.put_env(:memcache_client, :transcoder_decode_opts, [keys: :atoms])
     
-    assert %{test: "test"} == Memcache.Client.Transcoder.decode_value("{\"test\": \"test\"}", 0x0002)
+    assert {:ok, %{test: "test"}} == Memcache.Client.Transcoder.decode_value("{\"test\": \"test\"}", 0x0002)
 
     Application.delete_env(:memcache_client, :transcoder)
     Application.delete_env(:memcache_client, :transcoder_decode_opts)
@@ -47,7 +47,7 @@ defmodule Memcache.ClientTest.Transcoder do
   test "erlang transcoder encode_value" do
     Application.put_env(:memcache_client, :transcoder, Memcache.Client.Transcoder.Erlang)
     
-    {encoded, data_type} = Memcache.Client.Transcoder.encode_value(%{})
+    {:ok, encoded, data_type} = Memcache.Client.Transcoder.encode_value(%{})
 
     assert encoded == <<131, 116, 0, 0, 0, 0>>
     assert data_type == 0x0004
@@ -58,7 +58,7 @@ defmodule Memcache.ClientTest.Transcoder do
   test "erlang transcoder decode_value" do
     Application.put_env(:memcache_client, :transcoder, Memcache.Client.Transcoder.Erlang)
 
-    assert %{} == Memcache.Client.Transcoder.decode_value(<<131, 116, 0, 0, 0, 0>>, 0x0004)
+    assert {:ok, %{}} == Memcache.Client.Transcoder.decode_value(<<131, 116, 0, 0, 0, 0>>, 0x0004)
     
     Application.delete_env(:memcache_client, :transcoder)
   end
